c7bf206c037be07299da21f76964b83c17287c52
Firmiana-Unity <122654548+FirmianaI7z9@users.noreply.github.com>
2024-03-03 22:30:27 +0900

    Update information system (v1.10.2)

--- js/root_index.js
+++ js/root_index.js
@@ -1,16 +1,16 @@
 window.addEventListener('DOMContentLoaded', function() {
   var date = new Date();
   var d = {year:date.getFullYear(), month:date.getMonth() + 1, date:date.getDate()};
-  console.log(date);
-  console.log(new Date(1709305200));
+
+  // 本日の予定
 
   const home_todays_schedule_container = document.getElementById('htsc');
   var value = `<h3>本日 (${d.month}月${d.date}日) の予定</h3>`;
 
-  function load_file() {
+  function load_file(file_name) {
     const promise = new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest();
-      xhr.open('GET', `json/schedule.json`);
+      xhr.open('GET', file_name);
       xhr.addEventListener('load', (f) => resolve(xhr));
       xhr.send();
     });
@@ -19,9 +19,10 @@ window.addEventListener('DOMContentLoaded', function() {
 
   var cnt = 0;
   var dw = (home_todays_schedule_container.clientWidth) * 0.8 - 30;
-  load_file().then((xhr) => {
+  load_file('json/schedule.json').then((xhr) => {
     var data = JSON.parse(xhr.response);
     data['item'].forEach((item) => {
+      if (item.type == 'x') return;
       var a = d.year * 10000 + d.month * 100 + d.date;
       var ds = new Date(item.start_time);
       var df = new Date(item.finish_time);
@@ -33,10 +34,36 @@ window.addEventListener('DOMContentLoaded', function() {
         const ctx = document.createElement('canvas').getContext('2d');
         ctx.font = '19px "Segoe UI"';
         var w = ctx.measureText(item.text).width;
-        value += `<div class="si ${item.subject}"><p class="sd">${ds.getFullYear()}年<br>${ds.getMonth() + 1}月${ds.getDate()}日</p><p class="st"><span class="sl">${item.contest.join(' ')} | ${item.type == 'c' ? '大会情報' : '申し込み情報'}</span><br><span class="nr" style="width:${Math.max(100, 100 * w / dw)}%;transform:scalex(${Math.min(1,dw / w)});">${item.text}</span></p></div>`
+        value += `<div class="si ${item.subject}"><p class="sd">${ds.getFullYear()}年<br>${ds.getMonth() + 1}月${ds.getDate()}日</p><p class="st"><span class="sl">${item.contest.join(' ')} | ${item.type == 'c' ? '大会情報' : '申し込み情報'}</span><br><span class="nr" style="width:${Math.max(100, 100 * w / dw)}%;transform:scalex(${Math.min(1,dw / w)});">${item.text}</span></p></div>`;
       }
     });
     if (cnt == 0) value += '<p>本日の予定はありません。</p>';
     home_todays_schedule_container.innerHTML = value;
+  });
+
+  // 最新情報
+
+  const home_info_container = document.getElementById('htic');
+  var ivalue = '<h3>新着情報</h3>';
+
+  var icnt = 0;
+  load_file('json/info.json').then((xhr) => {
+    var data = JSON.parse(xhr.response);
+    data['item'].forEach((item) => {
+      var a = d.year * 10000 + d.month * 100 + d.date;
+      var ds = new Date(item.time);
+      var b = ds.getFullYear() * 10000 + (ds.getMonth() + 1) * 100 + ds.getDate();
+      if (Math.abs(a - b) > 5) return;
+      else {
+        icnt++;
+        var contest = '';
+        item.contest.forEach((con) => {
+          contest += `<span class="tg ${con.subject}">${con.contest}</span>`;
+        });
+        ivalue += `<div class="ni1">${item.link ? `<a href="${item.link_text}"${!item.domestic_link ? ' target="_blank" rel="noopener noreferrer"' : ''}></a>` : ''}<div>${item.important ? '<span class="nni"></span>' : ''}${a - b <= 5 ? '<span class="nnw"></span>' : ''}<p class="nt1">${contest}${item.text}<span class="nd1">${ds.getFullYear()}年${ds.getMonth() + 1}月${ds.getDate()}日</span></p></div></div>`;
+      }
+    });
+    if (icnt == 0) ivalue += '<p>5日以内のお知らせはありません。</p>';
+    home_info_container.innerHTML = ivalue;
   });
 });
---
+++ js/schedule.js
@@ -0,0 +1,51 @@
+window.addEventListener('DOMContentLoaded', function() {
+  var date = new Date();
+  var d = {year:date.getFullYear(), month:date.getMonth() + 1, date:date.getDate()};
+
+  const root_schedule_container = document.getElementById('rsc');
+  var value = '';
+  const ti = this.document.title.split('|')[1].substring(1, 3);
+  var dic = {'数学':'ma','物理':'ph','化学':'ch','生物':'bi','地学':'ea','情報':'in','科学':'ka','言語':'la','天文':'as','地理':'ge','哲学':'ps','経済':'ec','脳科':'ba'};
+
+  function load_file() {
+    const promise = new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      if (ti == 'スケ'){
+        xhr.open('GET', `json/schedule.json`);
+      }
+      else{
+        xhr.open('GET', `../../json/schedule.json`);
+      }
+      xhr.addEventListener('load', (f) => resolve(xhr));
+      xhr.send();
+    });
+    return promise;
+  }
+
+  var dw = document.getElementById('sc_sample').clientWidth - 10;
+  var cur_month = 0;
+  load_file().then((xhr) => {
+    var data = JSON.parse(xhr.response);
+    data['item'] = data['item'].sort(function(a, b){
+      return (a.start_time < b.start_time) ? -1 : 1;
+    });
+    data['item'].forEach((item) => {
+      if (ti != 'スケ' && item.subject != dic[ti]) return;
+      var a = d.year * 10000 + d.month * 100 + d.date;
+      var ds = new Date(item.start_time);
+      var b = ds.getFullYear() * 10000 + (ds.getMonth() + 1) * 100 + ds.getDate();
+      if (b < a) return;
+      else {
+        if (cur_month < ds.getFullYear() * 100 + (ds.getMonth() + 1)) {
+          value += `<h3 class="bh3">${ds.getFullYear()}年${ds.getMonth() + 1}月</h3>`;
+          cur_month = ds.getFullYear() * 100 + (ds.getMonth() + 1);
+        }
+        const ctx = document.createElement('canvas').getContext('2d');
+        ctx.font = '19px "Segoe UI"';
+        var w = ctx.measureText(item.text).width;
+        value += `<div class="si si1 ${item.subject}${item.type == 'x' ? ' cs' : ''}"><p class="sd">${ds.getFullYear()}年<br>${ds.getMonth() + 1}月${item.type == 'x' ? '' : ds.getDate().toString() + '日'}</p><p class="st"><span class="sl">${item.contest.join(' ')} | ${item.type == 'c' ? '大会情報' : (item.type == 'a' ? '申し込み情報' : 'その他・未確定情報')}</span><br><span class="nr" style="width:${Math.max(100, 100 * w / dw)}%;transform:scalex(${Math.min(1, dw / w)});">${item.text}</span></p></div>`;
+      }
+    });
+    root_schedule_container.innerHTML = value;
+  });
+});
