3fb45818f6dda7dc23eb8bbb0ba654faeb91833c
Firmiana-Unity <122654548+FirmianaI7z9@users.noreply.github.com>
2024-03-26 17:58:28 +0900

    Update schedule system (v1.10.28)

--- js/schedule.js
+++ js/schedule.js
@@ -24,6 +24,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
   var dw = document.getElementById('sc_sample').clientWidth - 20;
   var cur_month = 0;
+  var now = new Date();
   load_file().then((xhr) => {
     var data = JSON.parse(xhr.response);
     data['item'] = data['item'].sort(function(a, b){
@@ -44,7 +45,7 @@ window.addEventListener('DOMContentLoaded', function() {
         const ctx = document.createElement('canvas').getContext('2d');
         ctx.font = '19px sans-serif';
         var w = ctx.measureText(item.text).width;
-        value += `<div class="si si1 ${item.subject}${item.type == 'x' ? ' cs' : ''}"><p class="sd">${ds.getFullYear()}年<br>${ds.getMonth() + 1}月${item.type == 'x' ? '' : ds.getDate().toString() + '日'}</p><p class="st"><span class="sl">${item.contest.join(' ')} | ${item.type == 'c' ? '大会情報' : (item.type == 'a' ? '申し込み情報' : 'その他・未確定情報')}</span><br><span class="nr" style="width:${Math.ceil(Math.max(100, 100 * w / dw)) + 5}%;transform:scalex(${Math.floor(Math.min(100, 100 * dw / w)) * 0.01});">${item.text}</span></p></div>`;
+        value += `<div class="si si1 ${item.subject}${(item.type == 'x' || (now.getTime() > df.getTime() && (df.getHours() != 0 || df.getMinutes() != 0) && ds != df)) ? ' cs' : ''}"><p class="sd">${ds.getFullYear() != now.getFullYear() ? `${ds.getFullYear()}年<br>` : ''}${ds.getMonth() + 1}月${item.type == 'x' ? '' : ds.getDate().toString() + `日${(ds.getTime() != df.getTime() && ((ds.getHours() == 0 && ds.getMinutes() == 0))) ? '-' : ''}`}${(ds.getHours() != 0 || ds.getMinutes() != 0) ? `<br>${ds.getHours()}:${('00' + ds.getMinutes()).slice(-2)}${ds.getTime() == df.getTime() ? '' : '-'}` : ''}</p><p class="st"><span class="sl">${item.contest.join(' ')} | ${item.type == 'c' ? '大会情報' : (item.type == 'a' ? '申し込み情報' : 'その他・未確定情報')}</span><br><span class="nr" style="width:${Math.ceil(Math.max(100, 100 * w / dw)) + 5}%;transform:scalex(${Math.floor(Math.min(100, 100 * dw / w)) * 0.01});">${item.text}</span></p></div>`;
       }
     });
     root_schedule_container.innerHTML = value;
